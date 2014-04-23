e9419574d49dd978d1616654faf54f61827effb7
Philip Ingram <philip.d.ingram@gmail.com>
2014-04-23 14:44:53 -0700

    new cat form

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -2,7 +2,6 @@ class CatsController < ApplicationController
 
   def index
     @cats = Cat.all
-
   end
 
   def show
@@ -11,6 +10,29 @@ class CatsController < ApplicationController
       redirect_to cats_url
     else
       render :show
+    end
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      # flash[:errors] = @cat.errors.full_messages
+      redirect_to new_cat_url#, alert: "You're stuck here!"
+    end
+  end
+
+  def edit
+    @cat = Cat.find(cat_params)
+    if @cat.nil?
+      redirect_to cats_index
+    else
+      render :edit
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-10.times do |i|
+100.times do |i|
   birth_date = Date.new(2010, rand(12) + 1, rand(28)+1)
   Cat.create!(
                 name: "Cat#{i}",
